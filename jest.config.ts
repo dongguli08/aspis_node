b6411cc16760e95963d86cf8929df23ed6527956
donggun <hugh3232@naver.com>
2025-03-17 08:30:45 +0900

    chore: moduleNameMapper 설정

---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+export default {
+    moduleNameMapper: {
+      '^src/(.*)$': '<rootDir>/src/$1',  // src 경로 매핑
+    },
+    moduleFileExtensions: ['js', 'json', 'ts'],
+    rootDir: '.',
+    testRegex: '.*\\.spec\\.ts$',
+    transform: {
+      '^.+\\.(t|j)s$': 'ts-jest',
+    },
+  };
+  
